03cf1175b9fc1812380c87f7d84a0b374a81b4b8
TheKriserOne <dimazhgirov45632@gmail.com>
2025-04-18 17:19:59 -0700

    testing env

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,16 +1,18 @@
-
 import 'firebase/compat/auth';
 import firebase from "firebase/compat/app";
 
-const app = firebase.initializeApp({
-    apiKey: process.env.VITE_FIREBASE_API_KEY,
-    authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.VITE_FIREBASE_DATABASE_URL,
-    projectId: process.env.VITE_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.VITE_FIREBASE_APP_ID,
-    measurementId: process.env.VITE_FIREBASE_MEASUREMENT_ID
-});
+const app = firebase.initializeApp((() => {
+    console.log(process.env.dev);
+    return {
+        apiKey: process.env.VITE_FIREBASE_API_KEY,
+        authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN,
+        databaseURL: process.env.VITE_FIREBASE_DATABASE_URL,
+        projectId: process.env.VITE_FIREBASE_PROJECT_ID,
+        storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+        appId: process.env.VITE_FIREBASE_APP_ID,
+        measurementId: process.env.VITE_FIREBASE_MEASUREMENT_ID
+    };
+})());
 export const auth = app.auth();
 export default app
