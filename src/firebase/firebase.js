246e79575d4258471de3418ef6339c659128da96
TheKriserOne <dimazhgirov45632@gmail.com>
2025-05-07 15:18:43 -0700

    test

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,16 +3,7 @@ import firebase from "firebase/compat/app";
 
 const app = firebase.initializeApp(() =>
         {
-            console.log("Firebase config", {
-                apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-                authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-                databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
-                projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-                storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-                messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-                appId: import.meta.env.VITE_FIREBASE_APP_ID,
-                measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
-            });
+            console.log("Firebase config");
 return {
     apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
     authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
