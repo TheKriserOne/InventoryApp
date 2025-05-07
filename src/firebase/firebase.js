ca1c5cafd60b405cfd6f7d2df4ade509026b0677
TheKriserOne <dimazhgirov45632@gmail.com>
2025-05-07 15:09:06 -0700

    test

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,6 +2,6 @@
 import 'firebase/compat/auth';
 import firebase from "firebase/compat/app";
 
-const app = firebase.initializeApp(console.log("Firebase initialized"));
+const app = firebase.initializeApp(console.log(import.meta.env));
 export const auth = app.auth();
 export default app
